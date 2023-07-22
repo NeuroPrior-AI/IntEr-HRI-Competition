1b953e1d8c23d816fca81e44def62321deff5b62
ChrisRZZ <102701922+ChrisRZZ@users.noreply.github.com>
2023-07-22 14:01:22 +0800

    Delet_CNN_LSTM
    revised version

--- Simple.py
+++ Simple.py
@@ -1,102 +1,83 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 18 12:46:25 2023
+Created on Sun Jun 25 18:55:29 2023
 
 @author: zhezhengren
 """
 
-import os
 import numpy as np
 import pickle
-from sklearn.model_selection import KFold
-from sklearn.metrics import f1_score, confusion_matrix
-from imblearn.over_sampling import SMOTE
 from tensorflow.keras import utils as np_utils
 from pyriemann.estimation import XdawnCovariances
 from pyriemann.tangentspace import TangentSpace
+from pyriemann.utils.viz import plot_confusion_matrix
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import VotingClassifier
-from xgboost import XGBClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import AdaBoostClassifier
 from matplotlib import pyplot as plt
 from joblib import dump
-import seaborn as sns
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import f1_score  # import f1_score
 
-def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap='Blues'):
-    plt.figure(figsize=(10,7))
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        cm = np.around(cm, decimals=2)
-        cm[np.isnan(cm)] = 0.0
-    sns.heatmap(cm, annot=True, fmt='.2f', cmap=cmap, xticklabels=classes, yticklabels=classes)
-    plt.title(title)
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.savefig(os.path.join(folder_path, 'confusion_matrix.png'))
-    plt.show()
-
-subject_path='/Users/zhezhengren/Desktop/NeuroPrior_AI/Model_Competion/EEG/Data_Processing/New/data_by_subject/BY74D'
-
-with open(subject_path + '/X.pkl', 'rb') as f:
+all_data_path = '/Users/zhezhengren/Desktop/NeuroPrior_AI/Model_Competion/EEG/Data_Processing/New'
+with open(all_data_path + '/X.pkl', 'rb') as f:
     X = pickle.load(f)
-with open(subject_path + '/y.pkl', 'rb') as f:
+with open(all_data_path + '/y.pkl', 'rb') as f:
     y = pickle.load(f)
 y = np_utils.to_categorical(y-1)
+
+X_train, X_temp, Y_train, Y_temp = train_test_split(X, y, test_size=0.5)
+X_validate, X_test, Y_validate, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5)
 
 chans, samples = 64, 501
 n_components = 3
 
-clfs = [('mlp', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), MLPClassifier(hidden_layer_sizes=(50,), max_iter=1000, activation='relu', solver='adam', random_state=1))),
-        ('logreg', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), LogisticRegression(solver='liblinear'))),
-        ('svc', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), SVC(kernel='rbf', C=1, gamma=0.01, probability=True))),
-        ('rf', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), RandomForestClassifier(n_estimators=50))),
-        ('knn', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), KNeighborsClassifier(n_neighbors=5))),
-        ('xgb', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), XGBClassifier()))]
+clfs = [('mlp', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), MLPClassifier((150,), learning_rate='adaptive', batch_size=64))),
+        ('logreg', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), LogisticRegression(C=1.0))),
+        ('svm', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), SVC(kernel='rbf', C=10, gamma='scale', probability=True))),
+        ('rf', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), RandomForestClassifier(n_estimators=150))),
+        ('xgb', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), XGBClassifier(n_estimators=150, learning_rate=0.1))),
+        ('knn', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), KNeighborsClassifier(n_neighbors=5)))]
 
+# Grid search for hyperparameter tuning
 parameters = {'n_estimators': [100, 150, 200]}
 clf_xgb = GridSearchCV(XGBClassifier(), parameters)
 clfs.append(('xgb_gs', make_pipeline(XdawnCovariances(n_components), TangentSpace(metric='riemann'), clf_xgb)))
 
-clf = VotingClassifier(estimators=clfs, voting='soft')
+clf = VotingClassifier(estimators=clfs, voting='soft') 
 
-kf = KFold(n_splits=10, shuffle=True, random_state=42)
-validation_scores = []
-class_names = ['no error', 'S 96', 'S 80']
+X_train_flattened = X_train.reshape(X_train.shape[0], -1)
+X_test_flattened = X_test.reshape(X_test.shape[0], -1)
 
-i = 1
-for train_index, test_index in kf.split(X):
-    X_train, X_test = X[train_index], X[test_index]
-    Y_train, Y_test = y[train_index], y[test_index]
-    
-    X_train_flattened = X_train.reshape(X_train.shape[0], -1)
-    sm = SMOTE(random_state=42)
-    X_res, y_res = sm.fit_resample(X_train_flattened, Y_train.argmax(axis=-1))
-    X_res = X_res.reshape(X_res.shape[0], chans, samples)
+sm = SMOTE(random_state=42)
+X_res, y_res = sm.fit_resample(X_train_flattened, Y_train.argmax(axis=-1))
 
-    clf.fit(X_res, y_res)
-    preds_rg = clf.predict(X_test)
+X_res = X_res.reshape(X_res.shape[0], chans, samples)
 
-    acc2 = np.mean(preds_rg == Y_test.argmax(axis=-1))
-    f1 = f1_score(Y_test.argmax(axis=-1), preds_rg, average='macro')
-    validation_scores.append(acc2)
+clf.fit(X_res, y_res)
+dump(clf, 'Ensemble_simple.joblib')
+preds_rg = clf.predict(X_test)
 
-    folder_path = os.path.join(subject_path, f"Validation_{i}")
-    os.makedirs(folder_path, exist_ok=True)
+acc2 = np.mean(preds_rg == Y_test.argmax(axis=-1))
+print("Classification accuracy: %f " % (acc2))
 
-    with open(os.path.join(folder_path, 'accuracy.txt'), 'w') as f:
-        f.write(f"Classification accuracy: {acc2*100:.4f}%")  # As percentage
-    with open(os.path.join(folder_path, 'f1_score.txt'), 'w') as f:
-        f.write(f"F1 score: {f1*100:.4f}%")  # As percentage
+# Calculate and print F1 score
+f1 = f1_score(Y_test.argmax(axis=-1), preds_rg, average='weighted')  # use 'weighted' for multi-class problems
+print("F1 Score: %f" % f1)
 
-    cm = confusion_matrix(Y_test.argmax(axis=-1), preds_rg)
-    plot_confusion_matrix(cm, class_names, normalize=True, title='Normalized confusion matrix')
+names = ['no error', 'S 96', 'S 80']
+plt.figure(0)
+plot_confusion_matrix(preds_rg, Y_test.argmax(axis=-1), names, title='Ensemble Method')
+plt.savefig('cm_rg_simple.png')
 
-    dump(clf, os.path.join(folder_path, 'Ensemble.joblib'))
-
-    i += 1
+#cv_results = cross_validate(clf, X, y.argmax(axis=-1), cv=10)
+#print("10-fold cross validation accuracy: ", np.mean(cv_results['test_score']))
